e37d126802e43f88791cbd4f73167424af5999cb
Fabricio C Zuardi <fabricio@fabricio.org>
2017-06-02 23:35:07 -0300

    first login root script

---
+++ 2017-06-02-root.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "1. Change root password"
+passwd
+
+echo "2. Connect to internet"
+wifi-menu
+
+echo "2. Connect to internet"
+wifi-menu
+
+echo "3. Sync Pacman db"
+pacman -Syy
+
+echo "4. Upgrade system"
+pacman -Su
+
+echo "5. Install sudo"
+pacman -S sudo
+
+echo "6. Create admin user"
+useradd -m -G wheel -s /bin/bash fcz
+passwd fcz
+visudo
+
+echo "Finished"
